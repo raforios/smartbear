d423b5b6fa8f6a68e6e319c2a871a71cd436c2f9
raforios <raforios@gmail.com>
2026-09-23 02:10:47 -0400

    Cleaning REPO

--- frontend/utils/environment.py
+++
@@ -1,9 +0,0 @@
-'''
-    Environment Variables
-'''
-import os
-# from dotenv import dotenv_values
-
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
-# PARAMETERS = dotenv_values(os.path.join(os.path.dirname(os.path.dirname(BASEDIR)),
-#                                         'frontend', '.env'))
